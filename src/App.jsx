f276e04e98e373f8003fe8d855308adfca4ae15c
ajay980p <75460286+ajay980p@users.noreply.github.com>
2023-07-28 12:30:30 +0530

    working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listData, setListData] = useState('');
+  const [dataInArray, setDataInArray] = useState([]);
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * 100);
+  }
+
+  function getData() {
+    const randomID = getRandomInt(1, 100);
+    const newData = { id: randomID, note: listData };
+    setDataInArray([...dataInArray, newData]);
+  }
+
+  function removeData() {
+    setDataInArray([]);
+  }
 
   return (
-    <>
+    <div>
+      <h2>Todo List</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          name="note"
+          placeholder="Enter the Notes"
+          onChange={(e) => {
+            setListData(e.target.value);
+          }}
+        />
+        <button onClick={getData}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <h2>Data given are below</h2>
+        <button onClick={removeData}>Remove</button>
+
+        {dataInArray.length !== 0 &&
+          dataInArray.map((item) => {
+            return <p key={item.id}>{item.note}</p>;
+          })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
